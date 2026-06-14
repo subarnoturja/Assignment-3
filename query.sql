952c3016df2ed803a45f941f226e01739d12c79f
subarnoturja <subarnoturja15@gmail.com>
2026-06-15 01:52:16 +0530

    query 4 added

--- query.sql
+++ query.sql
@@ -36,3 +36,6 @@ select full_name, email from users where full_name ilike 'Tanvir%' or full_name 
 
 -- query 3
 select booking_id, user_id, match_id, coalesce(payment_status, 'Action Required') as systematic_status from bookings where payment_status is null; 
+
+-- query 4
+select booking_id, users.full_name, fixture, total_cost from bookings inner join matches on bookings.match_id = matches.match_id inner join users on bookings.user_id = users.user_id
